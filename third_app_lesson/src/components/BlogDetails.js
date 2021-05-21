1dd04fb5170ee9782e924496ba52770c99f6b493
mosemb <mosejava@gmail.com>
2021-05-21 23:26:28 +0200

    more updates

--- third_app_lesson/src/components/BlogDetails.js
+++ third_app_lesson/src/components/BlogDetails.js
@@ -1,12 +1,21 @@
 //import {useParams} from 'react-router-dom'// Helps us get route parameters for a particular route
 
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./usefetch";
 
 function BlogDetails(){
 
 const {id} = useParams();
 const {data_fe:blog, error, isPending} = useFetch("http://localhost:8000/blogs/"+id)
+const history = useHistory()
+
+function handleClickDelete(){
+    fetch("http://localhost:8000/blogs/"+id, {method:'DELETE'})
+    .then(
+        function(){
+            history.push("/")
+        })
+}
 
 
     return (
@@ -17,6 +26,7 @@ const {data_fe:blog, error, isPending} = useFetch("http://localhost:8000/blogs/"
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author}</p>
                 <div>{blog.body}</div>
+                <button onClick={handleClickDelete}>Delete</button>
                 </article>}
         </div>
 
--- third_app_lesson/src/components/Create.js
+++ third_app_lesson/src/components/Create.js
@@ -1,9 +1,65 @@
+import { useState } from "react";
+import {useHistory} from "react-router-dom"
 
 
 function Create(){
 
-    return (<div> 
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario")
+    const [ispending, setIsPending] = useState(false);
+    const history = useHistory(); // Helps us to use history 
+
+    const handleSubmit = function (e){
+
+        e.preventDefault(); // Prevents the automatic refresh of the page
+        const blog = {title, body, author} // This object returns the data for the title body and author
+        //console.log(blog)
+        setIsPending(true); // When the data is not added yet. 
+        fetch("http://localhost:8000/blogs", 
+        {
+            method: 'POST',
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(blog)
+        }).then(
+            function(){
+                console.log("New Blog added")
+                setIsPending(false); // When the data is added 
+                history.push("/") // Redirect user to home page after adding blog. 
+            }
+        )
+    }
+
+
+    return (<div className="create"> 
         <h1> Add a new Blog </h1>
+        <form onSubmit={handleSubmit}> 
+            <label> Blog Title:</label>
+            <input type = "text" 
+            required value= {title}
+            onChange={(e)=>setTitle(e.target.value)}
+            />
+
+            <label> Blog Body:</label>
+            <textarea required
+            value={body} 
+            onChange={(e)=>setBody(e.target.value)} ></textarea>
+
+            <label> Blog Author:</label>
+            <select value = {author}
+            onChange = {(e)=>setAuthor(e.target.value)}
+            >
+                <option value = "mario">Mario</option>
+                <option value = "yoshi">Yoshi</option>
+            </select>
+            {!ispending&& <button>Add Blog</button>}
+            {ispending&& <button disabled >Adding Blog ...</button>}
+
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
+
+        </form>
     </div>);
 
 }
